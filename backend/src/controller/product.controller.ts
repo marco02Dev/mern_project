16416eaa35c5fa59ab33a4b2eb3e35e8674af0d4
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-03-31 10:45:29 +0200

    Products controller and database functions updated

--- backend/src/controller/product.controller.ts
+++ backend/src/controller/product.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, ProductParams } from "../types/controller";
 import Product, {ProductSchema} from "../models/product.model";
-import { isProductDataInvalid } from "../utils/is-product-data-invalid";
-import { sendErrorMessage, SendErrorMessageData } from "../utils/send-error-massage";
-import { sendSuccessMessage, SendSuccessMessageData } from "../utils/send-success-message";
 import { getAllDocumentsByModel } from "../database/get-all-documents-by-model";
 import { createNewDocumentByModel } from "../database/create-new-document-by-model";
 import { deleteDocumentByModel } from "../database/delete-document-by-model";
+import { updateDocumentByModel } from "../database/update-document-by-model";
 import { Request } from "express";
 
 export const getAllProducts: Controller = async (req, res) => {
@@ -36,34 +34,10 @@ export const deleteProduct: Controller<ProductParams> = async (req, res) => {
 }
 
 export const updateProduct: Controller<ProductParams, {}, ProductSchema> = async (req, res) => {
-    const {id} = req.params;
-    if(id) {
-        const product: ProductSchema = req.body;
-        if(isProductDataInvalid(product)) {
-            const sendErrorMessageData: SendErrorMessageData = {response: res, statusCode: 400};
-            sendErrorMessage(sendErrorMessageData);
-            return;
-        } else {            
-            const {name}: ProductSchema = product;
-
-            try {
-                const ProductUpdated = await Product.findByIdAndUpdate(id, product, {new: true});
-                if(ProductUpdated) {
-                    const sendSuccessMessageData: SendSuccessMessageData = {
-                        response: res,
-                        statusCode: 200,
-                        data: ProductUpdated,
-                        resource: "Product",
-                        updateResource: true
-                    }
-                    sendSuccessMessage(sendSuccessMessageData);
-                    return;
-                } 
-            } catch {
-                console.log(`Product "${name}" not found`);
-                sendErrorMessage({response: res, statusCode: 404, resource: "Product"});
-                return;
-            }
-        }
-    }    
+    updateDocumentByModel<ProductSchema>({
+        Model: Product,
+        request: req,
+        response: res,
+        resourceName: "Product"
+    });
 }
---
+++ backend/src/database/update-document-by-model.ts
@@ -0,0 +1,81 @@
+import { isUserDataInvalid } from "../utils/is-user-data-invalid";
+import { isProductDataInvalid } from "../utils/is-product-data-invalid";
+import { sendErrorMessage } from "../utils/send-error-massage";
+import { sendSuccessMessage } from "../utils/send-success-message";
+import { Request, Response } from "express";
+import { Model } from "mongoose";
+import { UsersSchema } from "../models/users.model";
+import { ProductSchema } from "../models/product.model";
+import { ModelsAllowed } from "../types/models-allowed";
+
+type UpdateDocumentByModelData<T> = {
+    Model: Model<T>,
+    request: Request<{id: string}, {}, T>,
+    response: Response,
+    resourceName: string
+}
+
+type SendErrorData = {
+    response: Response,
+    documentName: string,
+    resourceName?: string
+} | {
+    response: Response,
+    documentName?: string,
+    resourceName: string    
+}
+
+const sendBadRequestError = ({response, documentName}: SendErrorData): void => {
+    console.log(`Bad request, ${documentName} not updated`)
+    sendErrorMessage({response: response, statusCode: 400});
+    return;
+}
+
+const sendNotFoundError = ({response, documentName, resourceName}: SendErrorData): void => {
+    const document: string = documentName ? documentName : "";
+    console.log(`${resourceName} "${document}" not found`);
+    sendErrorMessage({response: response, statusCode: 404, resource: "Product"});
+}
+
+export const updateDocumentByModel = async <T extends Object>({Model, request, response, resourceName}: UpdateDocumentByModelData<T>): Promise<void> => {
+    const {id}: {id: string} = request.params;
+    if(id) {
+        const clientData: any = request.body;
+        const isProduct: boolean = resourceName === "Product";
+        let documentName: string;
+
+        if(isProduct) {
+            if(isProductDataInvalid(clientData)) {
+                sendBadRequestError({response: response, resourceName: resourceName});
+            }
+            const {name}: ProductSchema = clientData
+            documentName = name;
+
+        } else {
+            if(isUserDataInvalid(clientData)) {
+                sendBadRequestError({response: response, resourceName: resourceName});
+            }
+            const {name}: UsersSchema = clientData
+            documentName = name;
+        }
+
+        try {
+            const DocumentUpdated: ModelsAllowed | null = await Model.findByIdAndUpdate(id, clientData, {new: true});
+            if(DocumentUpdated) {
+                sendSuccessMessage({
+                    response: response,
+                    statusCode: 200,
+                    data: DocumentUpdated,
+                    resource: resourceName,
+                    updateResource: true
+                });
+                return;
+            } 
+            } catch {
+                sendNotFoundError({response: response, documentName: documentName, resourceName: resourceName});
+                return;
+            }
+    } else {
+        sendNotFoundError({response: response, resourceName: resourceName})
+    } 
+}
