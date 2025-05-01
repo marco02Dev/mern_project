06cdee3c39c3ce315117ea57dfed9482832a7737
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-01 21:01:03 +0200

    Updated tsconfig package.json and vite-env.d.ts

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,2 +1,8 @@
-/// <reference types="vite/client" />
-/// <reference types="vite/types/importMeta.d.ts" />
+interface ImportMetaEnv {
+    readonly VITE_ENV: string;
+    readonly VITE_DEV_ENDPOINT: string;
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
