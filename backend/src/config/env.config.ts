4d1f4ecf6bbbbe5f1d529de2c4bb28c273623aa4
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-02 13:10:35 +0200

    Updated env.example and removed unecessary enviorment variables

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -17,10 +17,10 @@ export const authorizedIp = env.AUTHORIZED_IP;
 export const frontendUri = env.FRONT_END_URI;
 export const port: number = Number(env.PORT) || 8000;
 
-export const sessionEndpointName: string = env.SESSION_ENDPOINT_NAME || "session-init";
-export const productsEndpointName: string = env.PRODUCTS_ENDPOINT_NAME || "products";
-export const usersEndpointName: string = env.USERS_ENDPOINT_NAME || 'users';
-export const contactEndpointName: string = env.CONTACT_ENDPOINT_NAME || "contact";
+export const sessionEndpointName: string = "session";
+export const productsEndpointName: string = "courses";
+export const usersEndpointName: string = 'account';
+export const contactEndpointName: string = "contact-us";
 
 export const transporterData: {service: string, user: string | undefined, password: string | undefined} = {
     service: env.TRANSPORTER_SERVICE || "gmail",
