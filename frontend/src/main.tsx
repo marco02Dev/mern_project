ce0a4310d60353f5216f5752a6e8cbeccb5eb1b4
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-07 18:39:50 +0200

    Removed themeModeProvider component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeModeProvider } from './contexts/ThemeModeProvider.tsx';
 import { ResetCss } from './styles/reset-css.style.ts';
 import { FontStyles } from './styles/font.style.ts';
 import { GlobalStyles } from './styles/global.style.ts';
@@ -11,7 +10,6 @@ import { store } from './store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
     <ResetCss />
     <FontStyles />
     <GlobalStyles />
