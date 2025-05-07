8d2dc7796e862c4333cec58787691ac430c0fad7
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-07 22:06:36 +0200

    Add 404 image

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,6 +1,5 @@
 import { ReactElement } from "react";
 import { HeroSection } from "../components/sections/HeroSection";
-import { endpoints } from "../config/endpoints.config";
 import { useEffect } from "react";
 import { Dispatch } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
@@ -22,7 +21,7 @@ export const NotFoundPage = (): ReactElement => {
             eyebrowText="Oops!"
             title="Page Not Found"
             description="The page you're looking for doesn't exist or has been moved. Return to the homepage and try again."
-            imageSrc={`${endpoints.imagesEndpoint}/pages/homepage/hero-section.webp`}
+            imageSrc={"https://res.cloudinary.com/dqwoo44z8/image/upload/v1746648096/hero-image_qrfg8j.webp"}
             imageAlt="Illustration of a page not found"
             secondaryColor
             buttonLabel="Go to home page"
