420d0d3b5b7b21d1865cee1b8a5e227444f0bf64
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-10 14:30:05 +0200

    Update logOutService to redirect to client login page when admin logs out, fully exiting admin panel

--- frontend/src/services/log-out.service.ts
+++ frontend/src/services/log-out.service.ts
@@ -5,6 +5,8 @@ import { handleErrorResponse } from "../utils/form/handle-error-response";
 import { destroySessionCookie } from "../utils/cookies/destroy-session-cookie.util";
 
 export const logOutService: Service = async ({ dispatch, navigate }) => {
+    const isAdmin: boolean = window.location.pathname.startsWith("/admin");
+
     try {
         const response = await fetch(`${endpoints.usersEndpoint}/logout`, {
             method: 'POST',
@@ -19,7 +21,11 @@ export const logOutService: Service = async ({ dispatch, navigate }) => {
         destroySessionCookie();
 
         if(dispatch) dispatch(setLoggedOut());
-        if(navigate) navigate("/login");
+        if(isAdmin) {
+            window.location.href = "/login";
+        } else {
+            if(navigate) navigate("/login");
+        }
     } catch (error) {
         console.error(error);
         handleErrorResponse({ statusCode: 500 });
