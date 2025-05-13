24592c9c205074bae25bbcda974b7fecec4d9bc9
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-13 16:36:23 +0200

    Update CookieYes global styles

--- frontend/client/src/styles/cookie-yes.style.ts
+++ frontend/client/src/styles/cookie-yes.style.ts
@@ -31,10 +31,6 @@ export const CookieYesStyles: CookieYesStylesType = createGlobalStyle<CookieYesS
         color: ${({$textColor}) => $textColor} !important;
         background-color: ${({$backgroundColor}) =>  $backgroundColor} !important;  
     }
-    .detail-powered-by {
-        color: ${({$textColor}) => $textColor} !important;
-        background-color: ${({$secondaryBackgorundColor}) => $secondaryBackgorundColor} !important;
-    }
     .cky-title, .cky-notice-des, .cky-consent-bar, .cky-preference-title, .cky-accordion-btn, .cky-btn-customize::after, .cky-accordion-header-des, .cky-preference-content-wrapper p {
         color: ${({$textColor}) => $textColor} !important; 
     }
