5504d5b3e2dbcd11ae6be4e299dbe154c55a0f55
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-13 17:01:29 +0200

    Add Cookie Preferences link in footer

--- frontend/client/src/components/template-parts/Footer.tsx
+++ frontend/client/src/components/template-parts/Footer.tsx
@@ -4,6 +4,7 @@ import { StyledSpace } from "../themed/StyledSpace";
 import styled from "styled-components";
 import { getCurrentYear } from "../../utils/browser/get-current-year.util";
 import { FadeInWrapper } from "../animated/FadeInWrapper";
+import { StyledLink } from "../themed/StyledLink";
 import { ThemeColors, useThemeColors } from "../../hooks/theme/useThemeColors";
 
 const FooterWrapper = styled.footer<{$backgroundColor: string}>`
@@ -21,6 +22,17 @@ export const Footer: FC = (): ReactElement => {
     return <FooterWrapper $backgroundColor={backgroundColor}>
 
         <StyledSpace small vertical />
+
+        <FadeInWrapper>
+            <a href="#" className="cky-banner-element">
+                <StyledLink 
+                    content="Cookie Preferences" 
+                    onClickFunction={(event) => {
+                        event.preventDefault();
+                    }}
+                />
+            </a>
+        </FadeInWrapper>
 
         <FadeInWrapper>
             <StyledText
