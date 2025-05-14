570c47512d245a5409f885704454eb3d1c622faf
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-14 19:57:12 +0200

    docs(utils): add JSDoc for getCurrentYear function

--- frontend/shared/utils/browser/get-current-year.util.ts
+++ frontend/shared/utils/browser/get-current-year.util.ts
@@ -1,3 +1,16 @@
+/**
+ * Returns the current year as a string.
+ *
+ * This function retrieves the current year based on the system's date and returns it
+ * as a string.
+ *
+ * @returns {string} The current year in string format.
+ * 
+ * @example
+ * const currentYear = getCurrentYear();
+ * console.log(currentYear); // Output: "2025" (or the current year)
+*/
+
 export const getCurrentYear = (): string => {
     return String(new Date().getFullYear());
 };
