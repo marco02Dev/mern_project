d4d0e94bd36c3b1654fb6e7e0cc3b0d61e60fd02
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-14 15:14:38 +0200

    docs: add JSDoc comments to useDynamicTitle hook

--- frontend/client/src/hooks/navigation/useDynamicDocumentTitle.ts
+++ frontend/client/src/hooks/navigation/useDynamicDocumentTitle.ts
@@ -1,14 +1,26 @@
 import { useEffect } from "react";
 import { useLocation, Location } from "react-router-dom";
 import { capitalizeFirstLetter } from "../../utils/common/capitalize-first-letter.util";
-import { multiPageAppMode } from "../../config/app.config";
 
-export const useDynamicTitle = (suffix = "WebCourses") => {
+/**
+ * Custom hook that dynamically sets the document title based on the current path.
+ * It capitalizes the last segment of the URL and appends a suffix to create the title.
+ * 
+ * @param suffix The suffix to be appended to the page title (default is "WebCourses").
+ * 
+ * @returns void
+ * 
+ * @description
+ * This hook listens for changes in the URL path and updates the document title based
+ * on the last segment of the path. If the path has segments, it capitalizes the last 
+ * one and sets it as the title followed by the suffix. If there are no segments, it 
+ * sets the title to just the suffix.
+*/
+
+export const useDynamicTitle = (suffix = "WebCourses"): void => {
   const location: Location = useLocation();
 
   useEffect(() => {
-    if (multiPageAppMode) return;
-
     const pathSegments = location.pathname.split("/").filter(Boolean);
     const lastSegment = pathSegments.length > 0 ? pathSegments[pathSegments.length - 1] : null;
 
