6c178fcf37f9f6d8ab4240de0a82cac31e654ca4
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-15 18:42:02 +0200

    Update checkAthorizedIp

--- backend/src/middlewares/security/authorize-ip.middleware.ts
+++ backend/src/middlewares/security/authorize-ip.middleware.ts
@@ -3,17 +3,15 @@ import { NextFunction, Response, Request } from "express";
 import { sendErrorMessage } from "../../utils/send-error-massage.util";
 import { RequestHandler } from "express";
 
-export const checkAuthorizedIp: RequestHandler = (req, res, next) => {
-  const xForwardedFor = req.headers['x-forwarded-for'] as string | undefined;
-  const clientIp = xForwardedFor 
-    ? xForwardedFor.split(',')[0].trim()
-    : req.ip?.replace("::ffff:", "");
-
+export const checkAthorizedIp: RequestHandler = (request: Request<any>, response: Response, next: NextFunction) => {
+  const xForwardedFor = request.headers['x-forwarded-for'] as string | undefined;
+  const clientIp = xForwardedFor ? xForwardedFor.split(',')[0].trim() : request.ip?.replace("::ffff:", "");
   const authorizedIps = getAuthorizedIps();
 
-  console.log("Forwarded IP:", xForwardedFor);
-  console.log("Selected client IP:", clientIp);
+  console.log("Client IP:", clientIp);
   console.log("Authorized IPs:", authorizedIps);
+  console.log("Raw request.ip:", request.ip);
+  console.log("x-forwarded-for header:", xForwardedFor);
 
   if (clientIp && authorizedIps.includes(clientIp)) {
     console.log("Authorized IP:", clientIp);
@@ -21,7 +19,7 @@ export const checkAuthorizedIp: RequestHandler = (req, res, next) => {
   } else {
     console.warn("Unauthorized IP:", clientIp);
     sendErrorMessage({
-      response: res,
+      response: response,
       statusCode: 403
     });
   }
