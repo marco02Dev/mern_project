22baef60283d1c691655948acbf6fdfd5c87ab04
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-16 17:01:50 +0200

    Backend: Add Winston as dev dependecy and updated connectToDatabase function

--- backend/src/config/system/connect-to-database.config.ts
+++ backend/src/config/system/connect-to-database.config.ts
@@ -1,12 +1,13 @@
 import mongoose, { Mongoose } from "mongoose";
 import { databaseUri } from "../system/env.config";
+import logger from "../libraries/winston.config";
 
 export const connectToDatabase = async (): Promise<Mongoose | undefined> => {
     try {
         if (databaseUri && typeof databaseUri === 'string') {
             const connection: Mongoose = await mongoose.connect(databaseUri);
             const dbName = connection.connection.db?.databaseName;
-            console.log(`Database ${dbName} connection succesful`);
+            logger.info(`Database ${dbName} connection succesful`);
             return connection;
         } else {
             console.error('DATABASE_URI is not defined!');
@@ -14,8 +15,8 @@ export const connectToDatabase = async (): Promise<Mongoose | undefined> => {
         }
     } catch (error) {
         if(error instanceof Error) {
-            console.error('Error detected during DB connection: ', error.message);
-            console.error(error);  
+            logger.error('Error detected during DB connection: ', error.message);
+            logger.error(error);  
             return Promise.reject('Conncection refused');
         }
     }
