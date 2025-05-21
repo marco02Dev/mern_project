5635ea34f3c9a197aa11c589d650dbd126f94635
Marco Sacco <marcosacco@Mac.wind3.hub>
2025-05-21 15:13:24 +0200

    Add hover effect on StyledFooterLink component

--- frontend/shared/components/themed/StyledFooterLink.tsx
+++ frontend/shared/components/themed/StyledFooterLink.tsx
@@ -2,18 +2,27 @@ import { FC, ReactElement } from "react";
 import { StyledText } from "./StyledText";
 import styled from "styled-components";
 import { FooterLinkItem } from "@shared/config/footer-links.config";
+import { ThemeColors, useThemeColors } from "@shared/hooks/theme/useThemeColors";
 
-const Wrapper = styled.a`
-    text-decoration: none;
+const Wrapper = styled.a<{$hoverColor: string}>`
+  text-decoration: none;
+  &:hover {
+    p {
+      color: ${({$hoverColor}) => $hoverColor};
+    }
+  }
 `;
 
 export const StyledFooterLink: FC<FooterLinkItem> = ({ 
-    href, 
-    text, 
-    targetBlank, 
-    className 
-}: FooterLinkItem): ReactElement => (
-  <Wrapper
+  href, 
+  text, 
+  targetBlank, 
+  className 
+}: FooterLinkItem): ReactElement => {
+  const { hoverColor }: ThemeColors = useThemeColors();
+
+  return <Wrapper
+    $hoverColor={hoverColor}
     href={href}
     target={targetBlank ? "_blank" : undefined}
     rel={targetBlank ? "noopener noreferrer" : undefined}
@@ -21,4 +30,4 @@ export const StyledFooterLink: FC<FooterLinkItem> = ({
   >
     <StyledText tag="p" size="p" verySmallParagraph content={text} />
   </Wrapper>
-);
+}
